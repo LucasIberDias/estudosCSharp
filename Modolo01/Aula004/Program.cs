d9555d4f4c481402dca51c0f3c481e46c3a335ba
LucasIberDias <lucas.skuggar@gmail.com>
2025-12-08 09:19:04 -0300

    Aula004 concluida, nesta aula foi possível aprender sobre os tipos de dados inteiros, como short, int e long e seu tamanhos

---
+++ Modolo01/Aula004/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Tipos de dados inteiros
+        //3 tipos: short -> int -> long | Estes possuem sinais + - 
+        //3 tipos: ushort -> uint -> ulong | Não possuem sinais + -
+
+        short n1 = -20;
+        int n2 = 30;
+        long n3 = 40l;
+
+        ushort n4 = 20;
+        uint n5 = 30u;
+        ulong n6 = 40ul;
+
+        int tamanho = sizeof(int);//retorna o tamanho em bytes
+        Console.WriteLine(tamanho);
+    }
+}
