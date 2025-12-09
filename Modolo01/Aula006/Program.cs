6ff079694cad62a70ef01a67aa5dd0596c90fcb3
LucasIberDias <lucas.skuggar@gmail.com>
2025-12-09 08:47:32 -0300

    Aula006 concluida, nesta aula foi possivel observar dados e tipos de operadores e ao final maneiras de formatar os valores

---
+++ Modolo01/Aula006/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Aula006;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Operadores Aritméticos:
+        // + - / * %
+        int n1 = 5;
+        int n2 = 5;
+
+        int som = n1 + n2;
+        int sub = n1 - n2;
+        int div = n1 / n2;
+        int mul = n1 * n2;
+        int mod = n1 % n2;
+
+        //Concatenação
+        Console.WriteLine("Valor soma: "+som);
+
+        //Placeholders
+        Console.WriteLine("Divisão Inteira: {0} Resto de divisão: {1}",div,mod);
+
+        //Interpolação de strings
+        Console.WriteLine($"Valor Subtração: {sub}  Valor Multiplicação: {mul}");
+
+    }
+}
--- Modolo01/Aula006/obj/Debug/net10.0/Aula006.AssemblyInfo.cs
+++ Modolo01/Aula006/obj/Debug/net10.0/Aula006.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Aula002")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Aula006")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfc4f49d4a961f8c52cedc53bdc9efac15ba2303")]
-[assembly: System.Reflection.AssemblyProductAttribute("Aula002")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Aula002")]
+[assembly: System.Reflection.AssemblyProductAttribute("Aula006")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Aula006")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
