4e17d62b5998349fae39708529e1e1a09cfa450f
LucasIberDias <lucas.skuggar@gmail.com>
2025-12-11 08:57:18 -0300

    Aula009 concluida, nesta aula foi possivel observar como se faz a conversão de valores String para inteiro além disso a maneira em que convertemos as entradas de dados do usuario de maneira rápida

---
+++ Modolo01/Aula009/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula009;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Conversões
+
+        //Classe converte
+        string texto = "123";
+        int numero = Convert.ToInt32(texto);
+        Console.WriteLine(numero);
+
+        //Metodo Parse
+        string texto2 = "456";
+        int numero2 = int.Parse(texto2);
+
+        //Entradas do usuario
+        Console.WriteLine("Insira um número:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"O número inserido + 10 é: {n+10}");
+    }
+}
