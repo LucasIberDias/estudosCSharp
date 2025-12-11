34d030a9d3aa4cfedf7fe9584ccb96281aef8d48
LucasIberDias <lucas.skuggar@gmail.com>
2025-12-11 09:15:58 -0300

    Aula010 concluida possivel ver enumeradores e Dynamic que é um tipo de dado sem tipo definido posso alterar o quanto for necessario

---
+++ Modolo01/Aula010/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Aula010;
+
+class Program
+{
+    enum Dias{segunda, terça, quarta, quinta, sexta}
+    enum StatusPedido{espera = 0, pago = 1, cancelado = 2}
+    static void Main(string[] args)
+    {
+        //Enumeradores e dados dynamic
+        Dias dia_atual = Dias.sexta;
+        Console.WriteLine((int)dia_atual);
+
+        StatusPedido status = StatusPedido.pago;
+        Console.WriteLine(status);
+        Console.WriteLine((int)status);
+
+        //Dynamic é um dado sem tipo pode mudar durante execução
+        dynamic valor = 100;
+        Console.WriteLine(valor);
+        valor = "Agora é um texto";
+        Console.WriteLine(valor);
+        valor = true;
+        Console.WriteLine(valor);
+    }
+}
