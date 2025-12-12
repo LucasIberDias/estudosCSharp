9630c5dc18c4f2a9b4f1c364909392c8f6b3bb68
LucasIberDias <lucas.skuggar@gmail.com>
2025-12-12 09:27:28 -0300

    Aula012 concluida e exercicio para a prova também na próxima começo o modolo02

---
+++ Modolo01/Aula012/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Aula012;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Classe math
+
+        //Constantes
+        Console.WriteLine(Math.PI);
+        Console.WriteLine(Math.E);
+
+        //Valor absoluto / módulo
+        Console.WriteLine(Math.Abs(-10));
+
+        //Valor máximo e minimo
+        Console.WriteLine(Math.Max(10,20));
+        Console.WriteLine(Math.Min(10,20));
+
+        //Potencias e Raizes
+        Console.WriteLine(Math.Pow(2,3));
+        Console.WriteLine(Math.Sqrt(81));
+
+        //Arredondamentos
+        Console.WriteLine(Math.Round(3.5));
+        Console.WriteLine(Math.Ceiling(3.5));
+        Console.WriteLine(Math.Floor(3.5));
+
+        //Logaritmo e exponencial
+        Console.WriteLine(Math.Log(10));
+        Console.WriteLine(Math.Log10(100));
+        Console.WriteLine(Math.Exp(1));
+    }
+}
---
+++ Modolo01/Exercicios/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace exercicios;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*Escreva um programa que leia uma temperatura em Celsius e exiba-a convertida para
+        fahrenheit, com duas casas decimais.
+        Fórmula: (celsius * 9 / 5) + 32
+        */
+
+        Console.WriteLine("Digite a temperatura que deseja transformar: (°C)");
+        double grausCelsius =  Convert.ToDouble(Console.ReadLine());
+
+        double grausFahrenheit = (grausCelsius * 9/5 ) + 32;
+
+        Console.WriteLine($"{grausCelsius} °C é o mesmo que {grausFahrenheit:F2} °F ");
+    }
+}
