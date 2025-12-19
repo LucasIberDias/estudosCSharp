1d89867afe15cc206c8ce409906c9704e1a9e855
LucasIberDias <lucas.skuggar@gmail.com>
2025-12-19 08:53:00 -0300

    Aula003 concluida foi possivel colocar em pratica uma estrutura condicional utilizando IF

---
+++ Modolo02/Aula003/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Aula003;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite sua idade:");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        if (idade >= 18)
+        {
+            Console.WriteLine("Você já pode tirar carteira de motorista");
+        }else
+        {
+            Console.WriteLine("Você não pode tirar carteira de motorista");
+        }
+        
+    }
+}
