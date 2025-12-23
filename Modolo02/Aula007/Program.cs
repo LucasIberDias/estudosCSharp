ac5020ae268570c132ad6f85f5c1f460baea1645
LucasIberDias <lucas.skuggar@gmail.com>
2025-12-23 10:10:42 -0300

    Aula007 concluida

---
+++ Modolo02/Aula007/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Aula007;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("[1] Opção 1");
+        Console.WriteLine("[2] Opção 2");
+        Console.WriteLine("[3] Opção 3");
+
+        int opcao = Convert.ToInt32(Console.ReadLine());
+
+        switch (opcao)
+        {
+            case 1:
+            Console.WriteLine("Você escolheu a opção 1");
+            break;
+
+            case 2:
+            Console.WriteLine("Você escolheu a opção 2");
+            break;
+
+            case 3:
+            Console.WriteLine("Você escolheu a opção 3");
+            break;
+
+            default:
+            Console.WriteLine("Isto não é uma opção");
+            break;
+        }
+    }
+}
