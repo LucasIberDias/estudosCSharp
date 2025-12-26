605e5506cecc6a10489999e455f41ea377e75795
LucasIberDias <lucas.skuggar@gmail.com>
2025-12-26 09:12:36 -0300

    Aula002 e Aula003 concluida o while tem a mesma estrutura do java com nehuma diferença principal até o momento

---
+++ Modolo03/Aula002/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Aula002;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 0;
+        while (i < 10)
+        {
+            System.Console.WriteLine("Olá mundo");
+            i++;
+        }
+    }
+}
---
+++ Modolo03/Aula003/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula003;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 0;
+        while (i <= 10)
+        {
+            if (i == 9)
+            {
+                break;
+            }
+            if (i % 2 == 1)
+            {
+                i++;
+                continue;
+            }
+            System.Console.WriteLine(i);
+            i++;
+        }
+    }
+}
