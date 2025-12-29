46417b497671fc12004e2c07f5f61296e58ea2a6
LucasIberDias <lucas.skuggar@gmail.com>
2025-12-29 08:50:35 -0300

    Aula007 fiz um pouco diferente da aula, a diferença foi que apenas usei uma estrutura de repetição para preencher a matriz em vez de fazer manual

---
+++ Modolo03/Aula007/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[3, 3];
+
+            for (int linha = 0; linha < 3; linha++)
+            {
+                for (int coluna = 0; coluna < 3; coluna++)
+                {
+                    Console.WriteLine($"Digite o valor da posição [{linha},{coluna}]:");
+                    matriz[linha, coluna] = Convert.ToInt32(Console.ReadLine());
+                    System.Console.WriteLine();
+                }
+            }
+
+            for (int linha = 0; linha < 3; linha++)
+            {
+                for (int coluna = 0; coluna < 3; coluna++)
+                {
+                    Console.Write(matriz[linha, coluna] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
