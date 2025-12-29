73ce230e8c4e5406d8f12c39c9c60337153731c0
LucasIberDias <lucas.skuggar@gmail.com>
2025-12-29 08:26:32 -0300

    Aula005 concluida foi possivel observar o loop for, que continua mantendo a mesma estrutura do java

---
+++ Modolo03/Aula005/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Aula005;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            System.Console.WriteLine(i);
+        }
+    }
+}
