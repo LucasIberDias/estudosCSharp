6cede8738f3d6727304aeca73851a5d33343cbf9
LucasIberDias <lucas.skuggar@gmail.com>
2025-12-29 08:57:15 -0300

    Aula008 do modolo03 concluida nesta aula vi como funciona o meto foreach que pega os n de uma array

---
+++ Modolo03/Aula008/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Aula008;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int array = {1,2,3};
+        
+
+        foreach (int n in array)
+        {
+            System.Console.WriteLine(n);
+        }
+    }
+}
