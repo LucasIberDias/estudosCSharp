43f68e1354fa65aad79653fe68f63e0ba6d96c1c
LucasIberDias <lucas.skuggar@gmail.com>
2025-12-29 08:31:31 -0300

    Aula006 modolo03 concluida nesta aula observei como funciona vetores que também mantém a estrutura do java

---
+++ Modolo03/Aula006/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Aula006;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int [] vetor = new int [10];
+
+        vetor[0] = 2;
+        vetor[4] = 56;
+
+        for(int i = 0; i < vetor.Length; i++){
+            if (vetor[i] != 0)
+            {
+                System.Console.WriteLine(vetor[i]);
+            }
+        }
+    }
+}
