19c2d351584b76836e4d7ca842664860e34c2567
LucasIberDias <lucas.skuggar@gmail.com>
2025-12-31 08:19:42 -0300

    Aula004 concluida foi possivel observar como funciona uma passagem por referencia

---
+++ POO01/Aula004/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula004;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n1 = 20;
+        Somar(ref n1);
+        System.Console.WriteLine(n1);
+    }
+
+    static void Somar(ref int valor)
+    {
+        valor++;
+    }
+}
