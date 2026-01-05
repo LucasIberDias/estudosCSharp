7cede75d3a6064a2ac27d7f1c04a0c925c9d58ad
LucasIberDias <lucas.skuggar@gmail.com>
2026-01-05 08:31:04 -0300

    fim da aula001 do módolo POO02 onde foi possivel ver como é a criação de classes

---
+++ POO02/Aula001/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula001;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
+
+// Classes -> modificadores da classe -> public, private, static e etc
+    //Não definido = Public -> Não tem restrições
+
+public class Classe1
+{
+    //Membros -> Modificadores de acesso
+    int n1 = 20;
+
+    public void Metodo01()
+    {
+        System.Console.WriteLine("Olá mundo!");
+    }
+}
