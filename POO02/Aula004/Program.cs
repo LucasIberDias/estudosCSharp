32b5eb50205a6516f0ad56409e70b2ab4d4950c0
LucasIberDias <lucas.skuggar@gmail.com>
2026-01-07 10:44:19 -0300

    Aula004 do modolo POO02 concluida, esta aula mostrou como funciona utilizar mais de um construtor

---
+++ POO02/Aula004/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Aula004;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Felinos gato1 = new Felinos("Siames");
+        System.Console.WriteLine(gato1.raca + " " + gato1.vida);
+
+        Felinos gato2 = new Felinos("Laranja", 150);
+        System.Console.WriteLine(gato2.raca + " " + gato2.vida);
+    }
+
+    public class Felinos
+    {
+        public int vida;
+        public string raca;
+
+        public Felinos(string entradaRaca)
+        {
+            vida = 100;
+            raca = entradaRaca;
+        }
+
+        public Felinos(string entradaRaca, int entradaVida)
+        {
+            vida = entradaVida;
+            raca = entradaRaca;
+        }
+    }
+}
