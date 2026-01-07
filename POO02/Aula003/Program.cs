45452c609bfe4179aa0a0a17f4109d0237e8028b
LucasIberDias <lucas.skuggar@gmail.com>
2026-01-07 10:37:56 -0300

    Aula003 do modolo POO02 concluida vi sobre construtores

---
+++ POO02/Aula003/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Aula003;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Felinos gato1 = new Felinos("Siames");
+        System.Console.WriteLine(gato1.raca);
+    }
+
+    public class Felinos
+    {
+        public int vida;
+        public string raca;
+
+        public Felinos(string entradaRaca)
+        {
+            vida = 100;
+            raca = entradaRaca;
+        }
+    }
+}
