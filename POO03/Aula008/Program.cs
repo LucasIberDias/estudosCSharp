8f484e57d0c5080a7670fc11ec15cd0e065c6422
LucasIberDias <lucas.skuggar@gmail.com>
2026-03-02 10:18:55 -0300

    Aula008 conluida

---
+++ POO03/Aula008/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Aula008;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Classe1 obj = new Classe1();
+        Classe2 obj2 = new Classe2();
+
+        obj.Metodo();
+        obj2.Metodo();
+    }
+}
+
+class Classe1
+{
+    public virtual void Metodo()
+    {
+        System.Console.WriteLine("Olá, mundo!");    
+    }
+}
+
+class Classe2 : Classe1
+{
+    //Override permite redefinir oq tem dentro de virtual
+    public override void Metodo()
+    {
+        System.Console.WriteLine("Outra coisa!");
+    }
+}
--- POO03/Aula008/obj/Debug/net10.0/Aula008.AssemblyInfo.cs
+++ POO03/Aula008/obj/Debug/net10.0/Aula008.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Aula007")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Aula008")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0afba8aadf0067b8af45c33cc8386455617491e1")]
-[assembly: System.Reflection.AssemblyProductAttribute("Aula007")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Aula007")]
+[assembly: System.Reflection.AssemblyProductAttribute("Aula008")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Aula008")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
