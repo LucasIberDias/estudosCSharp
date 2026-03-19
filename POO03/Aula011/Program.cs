10379bff7c15a186484c9b3316b46f0dd9e97179
LucasIberDias <lucas.skuggar@gmail.com>
2026-03-19 11:01:10 -0300

    Aula011 concluida

---
+++ POO03/Aula011/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Aula011;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Est1 valores;
+
+        valores.n1 = 10;
+        
+    }
+}
+
+struct Est1
+{
+    public int n1;
+    public int n2;
+
+    public Est1(int n1, int n2)
+    {
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+}
