b471f0f866b64684bead79c3f6aa49ee9a5fdbdc
LucasIberDias <lucas.skuggar@gmail.com>
2026-03-23 10:42:51 -0300

    Aula012 concluida

--- Modolo01/Aula011/obj/Debug/net10.0/Aula011.AssemblyInfo.cs
+++ Modolo01/Aula011/obj/Debug/net10.0/Aula011.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula011")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f484e57d0c5080a7670fc11ec15cd0e065c6422")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10379bff7c15a186484c9b3316b46f0dd9e97179")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula011")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula011")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ POO03/Aula012/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Aula012;
+
+namespace Name
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
+namespace Outro
+{
+    
+}
+
+//objetivo do namespace serve para organizar e guardar o código possuindo escopo
+
