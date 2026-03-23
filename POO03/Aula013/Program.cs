1d7bbc8a12365caef795759ed76e75a65db8d19c
LucasIberDias <lucas.skuggar@gmail.com>
2026-03-23 10:57:46 -0300

    Aula013 e Aula014 concluida

---
+++ POO03/Aula013/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Aula013;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        op del1 = new op(Operations.Soma);
+
+        int res = del1(10,20);
+
+        System.Console.WriteLine(res);
+    }
+}
+
+delegate int op(int n1, int n2);
+
+class Operations{
+    public static int Soma(int n1, int n2)
+    {
+        return n1 + n2;
+    }
+
+    public static int Sub(int n1, int n2)
+    {
+        return n1 - n2;
+    }
+}
---
+++ POO03/Aula014/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Aula014;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Metodo(10);
+    }
+    static void Metodo(int n1)
+    {
+        if(n1 > 0)
+        {
+            System.Console.WriteLine(n1);
+            n1--;
+            
+            Metodo(n1);
+        }
+    }
+}
