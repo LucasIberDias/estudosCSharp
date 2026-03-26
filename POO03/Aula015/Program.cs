49e423bc36cd4d3d21423dc2e7f5cf257f3645eb
LucasIberDias <lucas.skuggar@gmail.com>
2026-03-26 16:20:14 -0300

    aula015 e aula016 concluidas

---
+++ POO03/Aula015/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Aula015;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n1 = 10;
+        int n2 = 0;
+
+        //tenta
+        try
+        {
+            System.Console.WriteLine(n1/n2);
+        }
+        catch
+        {
+            System.Console.WriteLine("Houve um erro ");
+        }
+        finally
+        {
+            System.Console.WriteLine("Fim do tratamento");
+        }
+    }
+}
+
---
+++ POO03/Aula016/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Aula016;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+
+    public static void Metodo<qualquer>(qualquer res)
+    {
+        System.Console.WriteLine(res);
+    }
+}
