cd4d307f0c8bb265da9fc86b1d9e9b54bf94ec15
LucasIberDias <lucas.skuggar@gmail.com>
2026-04-07 10:00:25 -0300

    Aula001 de algoritmos pronta

---
+++ ALGORITMOS/Aula001/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Aula001;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] vetor = new int[5]{3,7,1,3,9};
+        BubbleSort(vetor);
+
+        foreach(var item in vetor)
+        {
+            System.Console.WriteLine(item);
+        }
+    }
+
+    static void BubbleSort(int[] arr)
+    {
+        for(int i =0; i < arr.Length-1; i++)
+        {
+            for (int j = 0; j < arr.Length - i - 1; j++)
+            {
+                if (arr[j] > arr[j+1])
+                {
+                    int t = arr[j];
+                    arr [j] = arr[j+1];
+                    arr[j+1] = t;
+                }
+            }
+        }
+    }
+}
