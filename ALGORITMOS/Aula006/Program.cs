623a00e848dd8ca71ae5b58cb1c6595b1f6688cd
LucasIberDias <lucas.skuggar@gmail.com>
2026-04-15 09:28:43 -0300

    aula006 iniciada

---
+++ ALGORITMOS/Aula006/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Aula006;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> Lista1 = new List<int>();
+        Lista1.Add(10);
+        Lista1.Add(20);
+        
+    }
+}
